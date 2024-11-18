99d04e8858deae8d031e3509abc7a86b3d430547
biryukov.kirill <biryukoff.kirill@gmail.com>
2024-11-18 21:58:15 +0700

    Revert "Delete request.sql"
    This reverts commit 260328358a9040d6ad2b778900fcefab24e531d3.

--- request.sql
+++ request.sql
@@ -4,7 +4,10 @@
 --     URLList
 -- LIMIT 20;
 
-SELECT * from linkBetweenURL;
+-- SELECT * from URLText;
+
+SELECT * from search;
+-- INSERT INTO search (searchText) VALUES (''); DROP TABLE search; --');
 
 -- SELECT rowId FROM URLList WHERE URL = ''; DROP TABLE linkBetweenURL; --';
 
